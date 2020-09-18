7d2f781637f6bf6d38d13b9520923fb7ad0a7c3e
Dima <bondardima357@gmail.com>
2020-09-18 21:27:29 +0300

    добавления первой програми

---
+++ Lesson001/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class HelloWorld {
+
+
+    public static void main(String[] args) {
+	System.out.println("любое  число:");
+	Scanner scan = new Scanner(System.in);
+	int get = scan.nextInt();
+	System.out.println(get);
+    }
+}
+
+
